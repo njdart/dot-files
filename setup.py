cb5624cdec25b9089a20f074ab2bc36dbdee5bb4
Nicholas Dart <nicholasjdart@gmail.com>
2016-04-24 22:33:13 +0100

    added tint3 configs for dekstop, background should now be symlinked to ~/.config/background for feh to use

--- setup.py
+++ setup.py
@@ -56,7 +56,10 @@ configFiles = [
     ".xscreensaver",
     ".xinitrc",
     ".zshrc",
-    ".tint3rc"
+    ".tint3rc",
+    ".tint3rc.KOCHANSKI.left",
+    ".tint3rc.KOCHANSKI.center",
+    ".tint3rc.KOCHANSKI.right"
   ]),
   ("$HOME/.config/sublime-text-3/Packages/User/", [
     "Default (Linux).sublime-keymap",
