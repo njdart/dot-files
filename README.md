37a77aaa975685cdf05405e99b3f203657ed83d3
Nicholas Dart <nicholasjdart@gmail.com>
2017-02-19 16:54:45 +0000

    - Removed unused files (dwm, xscreensaver, etc)  - Moved some stuff to folders  - Switched from tint3 to polybar

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 #Dot Files
 =========
 
-I use Awesome wm on Arch linux on both my desktop and laptops, here are some configurations for them :)
+I use BSPWM on Arch linux on both my desktop and laptops, here are some configurations for them :)
 
-##Busy:
-![Busy](https://github.com/njdart/dot-files/blob/master/busy.png)
-##Quiet:
-![Quiet](https://github.com/njdart/dot-files/blob/master/quiet.png)
+I've tried to automate this as much as possible. Running the `setup.py` file (python3) should symblink everything into place. Running it as root should also get the job done for you, but will symblink things like `mpd.conf` into place too.
--- setup.py
+++ setup.py
@@ -5,122 +5,189 @@
 
 import os
 import os.path
-
-
+import glob
+import socket
+import sys
+import pwd
+import grp
 
 ERRORCOLOUR = "\033[91m"
 WARNINGCOLOUR = '\033[93m'
 NOCOLOUR = "\033[0m"
 
-# this souldnt be run as root
-if os.geteuid() == 0:
-  exit("This should NOT be run as root!")
-
-# a destination-source tuple of directories, with sources possibly containing multiple entries
-#
-# entries are described as:
-# 
-#  (destinationDirectory, [sources])
-# where usesUserHome is boolean True/False
-# and destination is a directory
-configFiles = [
-  ("/etc/X11/xorg.conf.d/", [
-    "10-keyboard.conf",
-    "50-synaptics.conf"
-  ]),
-  ("/etc/", ["mpd.conf"]),
-  ("/etc/lightdm/", [
-     "greeters",
-     "lightdm.conf"
-  ]),
-  ("$HOME/bin/", [
-    "backgrounds",
-    "wifi-repeater"
-  ]),
-  ("$HOME/.config/awesome/", [
-    "brilliant",
-    "icons",
-    "myWidgets.lua",
-    "rc.lua"
-  ]),
-  ("$HOME/.config/sxhkd/", ["sxhkdrc"]),
-  ("$HOME/.config/bspwm/", ["bspwmrc"]),
-  ("$HOME/.ncmpcpp/", ["ncmpcpp"]),
-  ("$HOME/", [
-    ".muttrc",
-    ".vimrc",
-    ".xbindkeysrc",
-    ".Xdefaults",
-    ".Xresources",
-    ".xprofile",
-    ".xscreensaver",
-    ".xinitrc",
-    ".zshrc",
-    ".tint3rc",
-    ".tint3rc.KOCHANSKI.left",
-    ".tint3rc.KOCHANSKI.center",
-    ".tint3rc.KOCHANSKI.right",
-    ".tint3rc.ARCHY"
-  ]),
-  ("$HOME/.config/sublime-text-3/Packages/User/", [
-    "Default (Linux).sublime-keymap",
-    "Preferences.sublime-settings"
-  ]),
-  ("$HOME/.config/sublime-text-2/Packages/User/", [
-    "Default (Linux).sublime-keymap",
-    "Preferences.sublime-settings"
-  ]),
-  ("$HOME/screenshots/", []), # this is only to satisfy the scrot alias in .zshrc
+# a list of tuples as such:
+#   (
+#       "Destination directory as string",
+#       [
+#           "file1",
+#           "file2",
+#           "*file_with_glob_*"
+#           ("file3", "destinationName")
+#       ]
+#       lambda file: False  # Optional test to perform. Returning anything other than true will prevent linking.
+#                           # Anything other than false will be given as a reason for skipping
+#   ]
+configs = [
+    ("/etc/X11/xorg.conf.d", [
+        "./xorg/*"
+    ],
+    lambda x: socket.gethostname() == "QUEEG500" or "Only for QUEEG500"), # Laptop Only
+    ("/etc", ["./mpd.conf"]),
+    ("$HOME/bin", [
+        "./bin/*"
+    ]),
+    ("$HOME/.config/sxhkd", ["./bspwm/sxhkdrc"]),
+    ("$HOME/.config/bspwm", ["./bspwm/bspwmrc"]),
+    ("$HOME/.ncmpcpp", [("./ncmpcpp", "config")]),
+    ("$HOME", [
+        "./.muttrc",
+        "./.vimrc",
+        "./.Xdefaults",
+        "./.Xresources",
+        "./.xprofile",
+        "./.zshrc"
+    ]),
+    ("$HOME", ["./.xbindkeysrc"], lambda x: socket.gethostname() == "QUEEG500" or "Only for QUEEG500"),
+    ("$HOME/.config/polybar", [
+        "./polybar/*"
+    ]),
+    ("$HOME/.config/sublime-text-3/Packages/User", [
+        "./sublime/*"
+    ]),
+    ("$HOME/screenshots", []), # this is only to satisfy the scrot alias in .zshrc
 ]
 
-for destinationSourceTuple in configFiles:
+# First, check for sudo
+sudo_user = None
 
-  destination = os.path.expandvars(destinationSourceTuple[0])
-  sources = destinationSourceTuple[1]
+if "SUDO_USER" in os.environ:
+    print(WARNINGCOLOUR, end="")
+    sudo_user = os.environ['SUDO_USER']
+    sudo_user_uid = pwd.getpwnam(sudo_user).pw_uid
+    sudo_user_gid = grp.getgrnam(sudo_user).gr_gid
+    print("Running as Root, Detected sudoer as {} (UID {}, GID {})".format(sudo_user, sudo_user_uid, sudo_user_gid))
 
-  print("Working on directory " + destination)
+    oldHome = os.environ["HOME"]
+    newHome = os.path.expanduser("~" + sudo_user)
 
-  try:
+    print("Continuing will change your home directory from {} to {} and chown to {}"
+        .format(oldHome, newHome, sudo_user))
 
-    #validate the directory, if it doesnt exist, create it and deal with any permission denied errors
+    answer = input("Continue? Or Run for [r]oot? (N/y/r) ").lower() or 'n'
+
+    if answer[0] == "y":
+        os.environ["HOME"] = newHome
+
+    elif answer[0] == "r":
+        pass
+
+    else:
+        sys.exit(0)
+
+    print(NOCOLOUR)
+
+# Now get to work on the configs
+for config in configs:
+
+    # Get the destination location and expand it, then create it if it doesn't exist
+    destination = os.path.expandvars(config[0])
     if not os.path.exists(destination):
       os.makedirs(destination)
+      # Chown if nessecary
+      if sudo_user:
+          os.chown(destination, sudo_user_uid, sudo_user_gid)
 
-    for source in sources:
-      fullSourcePath = os.getcwd() + "/" + source
-      sourceFileName = os.path.basename(fullSourcePath)
-      fullDestinationPath = destination + sourceFileName
-      givenWarning = False
+    sources = []
 
-      # print(fullSourcePath + " -> " + fullDestinationPath)
+    for source in config[1]:
 
-      if not os.path.exists(fullSourcePath):
-        print(ERRORCOLOUR + " ERROR, source file " + fullSourcePath + " does not exist" + NOCOLOUR);
-        continue;
+        rename = None
 
-      try:
-        if os.path.islink(fullDestinationPath):
-          print(WARNINGCOLOUR + "  => Link " + fullDestinationPath + " exists, linking to " + os.path.realpath(fullDestinationPath) + ", replacing" + NOCOLOUR);
-          givenWarning = True
-          os.unlink(fullDestinationPath)
-        elif os.path.isfile(fullDestinationPath):
-          print(ERRORCOLOUR + "  => File " + fullDestinationPath + " exists, ignoring" + NOCOLOUR);
-          continue;
+        needsRename = isinstance(source, tuple)
 
-        if not givenWarning:
-          print("  => Symlinking " + fullSourcePath + " to " + fullDestinationPath);
-        os.symlink(fullSourcePath, fullDestinationPath);
+        # If a rename is needed, expand the tupel
+        if needsRename:
+            (source, rename) = source
 
-      except OSError as e:
+        # Expand any globs
+        expandedGlobs = glob.glob(source)
+
+        # If we are to rename, but there was a glob that expanded to more than one item, ignore it as we can't rename
+        if len(expandedGlobs) > 1 and needsRename:
+            print(ERRORCOLOUR, end='')
+            print("  => Cannot use renames with GLOB that matches more than one file")
+            print("     Source {} matched with {}. Renaming to {} is not possible"
+                .format(source, expandedGlobs, rename))
+            print(NOCOLOUR, end='')
+            continue
+
+        for expandedGlob in expandedGlobs:
+            # get the absolute path
+            absPath = os.path.abspath(expandedGlob)
+            (_, name) = os.path.split(absPath)
+            sources.append((absPath, rename or name))
+
+    # Allow tests to be performed on each file, if no test is provided, set a default one
+    test = lambda x: True
+    if len(config) == 3:
+        test = config[2]
+
+    print("Working on directory {}".format(destination))
+
+    try:
+
+        for (source, rename) in sources:
+
+            testResult = test(source)
+            if testResult != True:
+                print(WARNINGCOLOUR, end='')
+                print("  => Skipping Source file {} {}"
+                    .format(source, (" Reason: " + testResult if testResult else "")))
+                print(NOCOLOUR, end='')
+                continue
+
+            # Check the source file exists
+            if not os.path.exists(source):
+                print(ERRORCOLOUR, end='')
+                print("  => ERROR, source file {} does not exist".format(source))
+                print(NOCOLOUR, end='')
+                continue
+
+            # Get the proposed destination. Any directores provided in the source file location are omitted
+            fullDestinationPath = os.path.join(destination, rename)
+
+            try:
+                if os.path.islink(fullDestinationPath):
+                    print(WARNINGCOLOUR, end='')
+                    print("  => Link {} exists, linking to {}, replacing"
+                        .format(fullDestinationPath, os.path.realpath(fullDestinationPath)))
+                    print(NOCOLOUR, end='')
+                    os.unlink(fullDestinationPath)
+                    os.symlink(source, fullDestinationPath)
+
+                elif os.path.isfile(fullDestinationPath):
+                    print(ERRORCOLOUR, end='')
+                    print("  => File {} exists, ignoring".format(fullDestinationPath))
+                    print(NOCOLOUR, end='')
+                    continue;
+
+                else:
+                    print("  => Symlinking {} to {} to ".format(source, fullDestinationPath))
+                    os.symlink(source, fullDestinationPath)
+
+            except OSError as e:
+                if e.errno == 13:
+                    print(ERRORCOLOUR, end='')
+                    print(ERRORCOLOUR + "    => ERROR: Permission denied to symlink {} to {}"
+                        .format(source, fullDestinationPath))
+                    print(NOCOLOUR, end='')
+                else:
+                    print(e)
+
+    except OSError as e:
         if e.errno == 13:
-          print(ERRORCOLOUR + "    => ERROR: Permission denied to symlink " + source + " to " + fullDestinationPath + NOCOLOUR)
-        else:
-          print(e)
-
-  except OSError as e:
-    # On linux, errno 13 seems to be invalid permissions (eg for )
-    if e.errno == 13:
-      print(ERRORCOLOUR + "  => ERROR: Permission denied to make directory " + destination + NOCOLOUR)
-    else :
-      print(e)
-
+            print(ERRORCOLOUR, end='')
+            print("  => ERROR: Permission denied to make directory {}".format(destination))
+            print(NOCOLOUR, end='')
+        else :
+            print(e)
